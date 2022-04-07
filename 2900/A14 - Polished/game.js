98eb6f1bfb5cb049f65f4dba2d1c4cc7a710cf2d
jjlaforest <jjlaforest@wpi.edu>
2022-04-07 19:19:23 -0400

    Revert "Merge branch 'main' of https://github.com/itzkurt102/IMGD-2900-BIG-Gamers"
    This reverts commit aa8d8c9690b1b1365dc61e294ef96cb07b4fd06d, reversing
    changes made to 82419646fb8bb7effb216045b3db9d9cc2f1e7fa.

--- 2900/A14 - Polished/game.js
+++ 2900/A14 - Polished/game.js
@@ -108,7 +108,6 @@ var G = {
     activeLevelMap: [],
     buttonHoverColor: 0xF1F8FB,
     buttonNormalColor: 0x8AA6A3,
-    buttonGlyphColor: PS.COLOR_GREEN,
     defaultText: "Click on a word spot to guess!",
 
 
